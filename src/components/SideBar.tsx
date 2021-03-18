651ecca480b391d1fab2373cc07600c7d06775a4
Jefferson Wesley Macedo dos Santos <jefferson.wesley@ituran.com.br>
2021-03-17 22:34:27 -0300

    Challenge completed.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,44 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ISideBar {
+  selected: number;
+  onClickButton(id: number): void;
+}
+
+export function SideBar({ selected, onClickButton }: ISideBar) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selected === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
   // Complete aqui
 }
